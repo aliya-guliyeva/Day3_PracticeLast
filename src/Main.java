fb327087f8bf7fb4bc9631de76f94fa90c89455f
Aliyaguliyeva <aliya.1988@gmail.com>
2020-11-12 10:28:28 -0500

    last practice

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("HelloGit Last Day");
+
+
+
+
+
+    }
+}
